34854e43e4538e11797b12aa15bcfc153f2f7738
Adam Winnipass <adams4lyf@gmail.com>
2023-03-09 17:33:37 +0000

    feat: add models, migrations and seeds

---
+++ app/Http/Controllers/CourierShippingOptionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CourierShippingOptionController extends Controller
+{
+    //
+}
---
+++ app/Models/CourierShippingOption.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourierShippingOption extends Model
+{
+    use HasFactory;
+}
